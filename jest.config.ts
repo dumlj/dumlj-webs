4025b82ed69a4ed3cbe6c716681c2be808a74add
zhongjiahao <zhongjiahao@joyy.com>
2024-03-12 23:16:39 +0800

    feat: first commit

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,7 @@
-import path from 'path'
 import type { Config } from '@jest/types'
 import { workspaces } from './package.json'
 
 const TIMEOUT = 30e3
-const { packages } = workspaces
 
 process.env.NODE_OPTIONS = '--experimental-vm-modules'
 
@@ -15,15 +13,11 @@ export default async (): Promise<Config.InitialOptions> => {
     collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/*.d.ts'],
     projects: [
       /**
-       * 规范检测
-       */
-      path.join(__dirname, '__standardstests__/jest.config.standards.ts'),
-      /**
        * 请确保文件夹中至少拥有一个配置，否则会报错
        * Error: Can't find a root directory while resolving a config file path.
        */
       // prettier-ignore
-      ...packages.filter((path) => -1 === path.indexOf('__')).flatMap((path) => [
+      ...workspaces.filter((path) => -1 === path.indexOf('__')).flatMap((path) => [
         `<rootDir>/${path}/jest.config.unittest.ts`,
         `<rootDir>/${path}/jest.config.typetest.ts`,
       ]),
